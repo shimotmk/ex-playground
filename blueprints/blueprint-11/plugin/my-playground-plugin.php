96d1e1257e3bdec24c5dc1fc6bb0f0004ddb273d
shimotmk <shimotmk1104@gmail.com>
2025-04-06 16:42:47 +0900

    u

--- blueprints/blueprint-11/plugin/my-playground-plugin.php
+++ blueprints/blueprint-11/plugin/my-playground-plugin.php
@@ -3,8 +3,7 @@
 Plugin Name: My Playground Plugin
 */
 
-function my_custom_plugin()
-{
+function my_custom_plugin() {
 	echo '<h1>Hello from My Custom Plugin!</h1>';
 }
 add_action('admin_notices', 'my_custom_plugin');
