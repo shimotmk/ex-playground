fe0ead1633602ac2a8734da8a7266a1553fd29fc
shimotmk <shimotmk1104@gmail.com>
2025-05-11 14:50:40 +0900

    first commit

---
+++ blueprints/12/my-playground-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: My Playground Plugin
+*/
+
+function my_custom_plugin() {
+	echo '<h1>Hello from My Custom Plugin!</h1>';
+}
+add_action('admin_notices', 'my_custom_plugin');
